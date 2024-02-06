0d95bfb80a64cb711e03abe4d0115106423f7a7f
LeonardSuli <leonardsuli1991@gmail.com>
2024-02-06 22:01:41 +0100

    add 10 emails in page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default{
+  name: 'App',
+
+  data(){
+    return{
+
+      emails: []
+    }
+  },
+
+  created(){
+
+    // axios
+    // .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    // .then((response) => {
+    //   console.log(response);
+    //   this.email = response.data.response
+    // })
+
+    for (let i = 0; i < 10; i++) {
+
+      axios
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response) => {
+      console.log(response);
+      this.emails.push(response.data.response)
+    })
+
+    }
+    
+  }
+
+}
+
+
+
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div>{{ emails }}</div>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
